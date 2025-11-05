2216331a4b333d6131490813e30589fddce4ef88
Nande929 <nandenqeto9@gmail.com>
2025-11-05 20:53:11 +0200

    Added JavaScript and API

---
+++ index.js
@@ -0,0 +1,73 @@
+function displayClock () {
+    //Berlin
+    let berlinElement = document.querySelector("#berlin");
+    let berlinDateElement = berlinElement.querySelector(".date");
+    let berlinTimeElement = berlinElement.querySelector(".time");
+    let berlinTime = moment().tz("Europe/Berlin");
+
+
+    berlinDateElement.innerHTML = berlinTime.format("MMMM Do YYYY");
+    berlinTimeElement.innerHTML = berlinTime.format("HH:mm");
+
+    //Nairobi
+
+    let nairobiElement = document.querySelector("#nairobi");
+    let nairobiDateElement = nairobiElement.querySelector(".date");
+    let nairobiTimeElement = nairobiElement.querySelector(".time");
+    let nairobiTime = moment().tz("Africa/Nairobi");
+
+
+    nairobiDateElement.innerHTML = nairobiTime.format("MMMM Do YYYY");
+    nairobiTimeElement.innerHTML = nairobiTime.format("HH:mm");
+
+    //Johannesburg
+    let johannesburgElement = document.querySelector("#johannesburg");
+    let johannesburgDateElement = johannesburgElement.querySelector(".date");
+    let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+    let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+
+    johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+    johannesburgTimeElement.innerHTML = johannesburgTime.format("HH:mm");
+
+    //Denver
+    let denverElement = document.querySelector("#denver");
+    let denverDateElement = denverElement.querySelector(".date");
+    let denverTimeElement = denverElement.querySelector(".time");
+    let denverTime = moment().tz("America/Denver");
+
+
+    denverDateElement.innerHTML = denverTime.format("MMMM Do YYYY");
+    denverTimeElement.innerHTML = denverTime.format("HH:mm");
+}
+
+
+
+function displayWeather (response, cityTemp, cityIcon) {
+
+    let weatherIcon = document.querySelector(cityIcon);
+    let berlinTemp = document.querySelector(cityTemp);
+    console.log(response.data)
+    weatherIcon.innerHTML = `<img src="${response.data.condition.icon_url}">`; 
+    berlinTemp.innerHTML = `${Math.round(response.data.temperature.current)}&deg;`
+   
+}
+ 
+function getCity(city, cityTemp, cityIcon) {
+
+    let apiKey = "e2b85d39ocbf2atcfebaabf3b2422057";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+
+    axios.get(apiUrl).then(function(response) {
+    displayWeather(response, cityTemp, cityIcon)
+    });
+
+}
+getCity("Berlin", "#berlin-temp", "#berlin .weather-icon");
+getCity("Nairobi", "#nairobi-temp", "#nairobi .weather-icon");
+getCity("Johannesburg", "#johannesburg-temp", "#johannesburg .weather-icon");
+getCity("Denver", "#denver-temp", "#denver .weather-icon");
+
+
+setInterval(displayClock, 1000);
